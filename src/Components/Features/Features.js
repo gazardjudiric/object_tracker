2601e65b52c6a8c1d43470fbf5f2ff286853b3ae
GAZARD Judiric <gazardjudiric@gmail.com>
2023-04-24 06:20:18 +0100

    ajout de la video

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { IconesBookmarkStarFill } from "../../Assets/Icones/Icones";
-import PhoneFeatures from "../../Assets/Images/phone-features.png";
+// import PhoneFeatures from "../../Assets/Images/phone-features.png";
 import { FeaturesList } from "./data.js";
 import Feature from "./Feature";
+import ObjectTrackerVideo from "./Video/objetTrackerVideo.mp4";
 
 function Features(props) {
   return (
@@ -14,7 +15,10 @@ function Features(props) {
         </div>
         <div className="feature-content">
           <div className="fc-left">
-            <img src={PhoneFeatures} alt="Phone" />
+            {/* <img src={PhoneFeatures} alt="Phone" /> */}
+            <video width="550" height="400" controls className="video">
+              <source src={ObjectTrackerVideo} type="video/mp4"></source>
+            </video>
           </div>
           <div className="fc-right">
             {FeaturesList.map((feature, index) => (
